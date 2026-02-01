1f0cbe207cecc92ac087a87902859a283f79513d
sadamakhtar33-hub <sadamakhtar33@gmail.com>
2026-02-01 11:42:10 +0500

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,1 +1,22 @@
+import runpod
+import numpy as np
+import soundfile as sf
+import uuid
 
+def handler(event):
+  text = event["input"].get("text","")
+
+  if text == "":
+    return {"error": "Text is required"}
+
+#fake audio for testing
+
+audio = np.zeros(24000)
+
+filename = f"tmp/
+{uuid.uuid4()}.wav"
+sf.write(filename, audio, 24000)
+
+return {"message": "Server works", "audiop_path": filename
+       }
+runpod.serverless.start({"handler": handler})
